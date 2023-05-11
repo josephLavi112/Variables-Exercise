c2ee93324f121e9d19cc6af617ce0dbe28683692
Joseph LaVia <josephdlavia@gmail.com>
2023-05-11 13:12:18 -0500

    Variable exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+
+string name = "Joseph";
+int age = 25;
+char middleIntial = 'D';
+bool isOver21 = true;
+double currentTemp = 75.5;
+decimal currentPrice = 105.56m;
+
+Console.WriteLine($"Hello, My name is {name}. I am {age} years old. My middle name starts with {middleIntial}. Am I over the age of 21? {isOver21}. The temperature outside is {currentTemp} The price of eggs are {currentPrice} a dozen.");
+
+
